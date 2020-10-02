d0aba21b3d7c1890fca9877e853048cac47691d5
mozinomer <mozinomer12@gmail.com>
2020-10-02 23:57:33 +0500

    ad

--- Home-page.php
+++ Home-page.php
@@ -31,6 +31,59 @@
 		</div>
 	</div>
 </div>
+<div class="section3homepage">
+	<div class="container">
+		<h6>
+			<?php the_field('sub_headingss'); ?>
+		</h6>
+		<h3>
+			<?php the_field('heading_main'); ?>
+		</h3>
+		<p>
+			<?php the_field('contentsda'); ?>
+		</p>
+		<div class="row">
+			<div class="col">
+				<div class="contentLeft" id="boxcontainersection3">
+					<h5>
+						<?php the_field('headingaqwe'); ?>
+					</h5>
+					<ul>
+						<?php
+						if( have_rows('list_items1') ):
+						    while( have_rows('list_items1') ) : the_row();?>
+								<li>
+									<img src="<?php echo get_template_directory_uri(); ?>/image/9.png">
+									<?php the_sub_field('list_items'); ?>
+								</li>
+						    <?php endwhile;
+						else :
+						endif; ?>
+					</ul>
+				</div>
+			</div>
+			<div class="col">
+				<div class="contentRight" id="boxcontainersection3">
+					<h5>
+						<?php the_field('headingaqwe_2'); ?>
+					</h5>
+					<ul>
+						<?php
+						if( have_rows('list_items1_2') ):
+						    while( have_rows('list_items1_2') ) : the_row();?>
+								<li>
+									<img src="<?php echo get_template_directory_uri(); ?>/image/9.png">
+									<?php the_sub_field('list_items2'); ?>
+								</li>
+						    <?php endwhile;
+						else :
+						endif; ?>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 <div class="section2homepage">
 	<div class="container">
 		<div class="row alignC">
@@ -58,63 +111,6 @@
 		</div>
 	</div>
 </div>
-<div class="section3homepage">
-	<div class="container">
-		<h6>
-			See The difference
-		</h6>
-		<h3>
-			MXene Vs Graphene Oxide
-		</h3>
-		<p>
-			Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-		</p>
-		<div class="row">
-			<div class="col">
-				<div class="contentLeft" id="boxcontainersection3">
-					<h5>
-						MXene
-					</h5>
-					<ul>
-						<li>
-							10X Higher Practical Conductivity Than Reduced Graphene Oxide
-						</li>
-						<li>
-							Processing Requirements Suited for Volume Production
-						</li>
-						<li>
-							Unique Lithophilic Properties Promote Fast Lithium Ion Transport, and Prevents Dendrite Formation
-						</li>
-						<li>
-							Easily Modifiable Surface Chemistry
-						</li>
-					</ul>
-				</div>
-			</div>
-			<div class="col">
-				<div class="contentRight" id="boxcontainersection3">
-					<h5>
-						Graphene Oxide
-					</h5>
-					<ul>
-						<li>
-							Single Layer Graphene Sheets Production technology not Scalable 
-						</li>
-						<li>
-							“Graphene” available in powdered form is actually Reduced Graphene Oxide (rGO)
-						</li>
-						<li>
-							Functional Conductivity of rGO is several orders of magnitude less than single layer graphene.
-						</li>
-						<li>
-							Caustic Oxidization Synthesis Processes for rGO
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
 <div class="featureslist" style="background-image: url('<?php echo get_template_directory_uri(); ?>/image/1.png');">
 	<div class="container">
 		<h6>
@@ -134,41 +130,41 @@
 			</div>
 			<div class="col-md-4">
 				<div class="boxcontainer">
-					<span class="number">1</span>
+					<span class="number">2</span>
 					<p>
-						Fusion of Titanium Dioxide, Aluminum and Carbon Powders
+						 Etch Titanium Aluminum Carbide with HCL and Fluoride Salt, Stirring
 					</p>
 				</div>
 			</div>
 			<div class="col-md-4">
 				<div class="boxcontainer">
-					<span class="number">1</span>
+					<span class="number">3</span>
 					<p>
-						Fusion of Titanium Dioxide, Aluminum and Carbon Powders
+						Rinse and filter to Neutral PH with DI water (dewaters to 40% water content)
 					</p>
 				</div>
 			</div>
 			<div class="col-md-4">
 				<div class="boxcontainer">
-					<span class="number">1</span>
+					<span class="number">4</span>
 					<p>
-						Fusion of Titanium Dioxide, Aluminum and Carbon Powders
+						Remove excess water, through press filtration (dewaters to 10% water content)
 					</p>
 				</div>
 			</div>
 			<div class="col-md-4">
 				<div class="boxcontainer">
-					<span class="number">1</span>
+					<span class="number">5</span>
 					<p>
-						Fusion of Titanium Dioxide, Aluminum and Carbon Powders
+						Drying under heated vacuum (dewaters to  < 0.1 % water content)
 					</p>
 				</div>
 			</div>
 			<div class="col-md-4">
 				<div class="boxcontainer">
-					<span class="number">1</span>
+					<span class="number">6</span>
 					<p>
-						Fusion of Titanium Dioxide, Aluminum and Carbon Powders
+						Airtight Packaging
 					</p>
 				</div>
 			</div>
@@ -178,15 +174,15 @@
 <div class="surfacesection">
 	<div class="container">
 		<h6>
-			Inner process
+			<?php the_field('sub_headingsurface'); ?>
 		</h6>
 		<h3>
-			Mxene Surface Modification
+			<?php the_field('headingsurface'); ?>
 		</h3>
 		<p>
-			Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+			<?php the_field('contetnsurface'); ?>
 		</p>
-		<img src="<?php echo get_template_directory_uri(); ?>/image/2.png">
+		<img src="<?php the_field('image_surface'); ?>">
 	</div>
 </div>
 <div class="testtmonials">
