4929cfbcf6b9b398d73ac45a7f420bdced2c6743
mozinomer <mozinomer12@gmail.com>
2020-10-02 11:07:06 +0500

    asd

---
+++ about.php
@@ -0,0 +1,237 @@
+<?php /* Template Name: About Page */ get_header(); ?>
+
+
+<div class="aboutBanner" style="background-image: url('<?php echo get_template_directory_uri(); ?>/image/3.png');">
+	<div class="container">
+		<h2>
+			<?php the_title(); ?>
+		</h2>
+		<div class="breadcrumbs">
+			<a href="<?php echo site_url(); ?>">Home</a>
+			<span><?php the_title(); ?></span>
+		</div>
+	</div>
+</div>
+
+<div class="aboutcontent">
+	<div class="container">
+		<div class="row" id="section2about">
+			<div class="col-md-4">
+				<h6>
+					Our STory
+				</h6>
+				<h4>
+					When We Started <br> Our Jouney
+				</h4>
+			</div>
+			<div class="col-md-8">
+				<p>
+					MXenes are a two-dimensional (2D) material Discovered in 2011 by researchers at Drexel University. The term MXene is pronounced MAX-een. The name is derived from parent material, the MAX phase carbide, and after graphene due to its similar 2D structure. 
+				</p>
+			</div>
+		</div>
+		<img src="<?php echo get_template_directory_uri(); ?>/image/4.png">
+		<div class="row" id="contentaboutsection3">
+			<div class="col">
+				<p>
+					It is important to note that MXenes are a family of materials.  The parents are MAX phase Carbides or Nitrides which have a planar molecular structure. The MAX title from the parent material is an acronym that references the periodic table of elements. The “M” designates a Transition Metal, of which there are many, like Titanium, Vanadium, Molybdenum …etc.  The “A” Denotes an Element, most commonly Aluminum or Silicon, but also possible “A” elements are Tin, Arsenic, Germanium, Gallium. The “X” represents a single or up to three Carbon or Nitrogen atoms, hence the Carbide/Nitride title. 
+				</p>
+			</div>
+			<div class="col">
+				<p>
+					Why is the name “MXene” missing the “A”?  The “A” element is removed through chemical processes leaving the Transition Metal “M” and the Carbide/Nitride “X”. To date, over 20 types of 2D MXenes have been successfully been synthesized, while many more remain theoretical possibilities. The most common MXenes are Titanium Carbides.
+				</p>
+			</div>
+		</div>
+		<div class="section2homepage">
+			<div class="container">
+				<div class="row alignC">
+					<div class="col-md-6">
+						<div class="imagecontainersection2Hoepage">
+							<img src="<?php echo get_template_directory_uri(); ?>/image/5.png">
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="contentSection2">
+							<h4>
+								The best of a kind
+							</h4>
+							<p>
+								Since its discovery, significant research has been dedicated to MXene research. Applications extremely unique properties of MXenes are still being discovered. The Excellent functional conductivity is a useful properties of the Mxenes.  The functional conductivity is 1-2 orders of magnitude higher than that of any  films, paper, or tube comprised of graphene oxide (GO) or Reduced Graphene Oxide (rGO). 
+							</p>
+							<a href="<?php the_field('button_linkSection2'); ?>">
+								Read More
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="forged">
+		<div class="container">
+			<div class="forgedinner">
+				<div class="contentforged">
+					<h6>
+						Our Vision
+					</h6>
+					<h4>
+						Forged by the Sea
+					</h4>
+					<p>
+						MXenes significantly improve upon a wide array of technologies. There is an opportunity to commercialize the upon technological improvements that can be attained by MXene applications. The Goal of Ultrion Energy is to reduce the cost and increase the availability of MXenes which will enable economic commercialization of MXene Technologies.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="testtmonials">
+		<div class="container">
+			<div class="slidertestimonials">
+				<div class="slidetest">
+					<h6>
+						Our Team
+					</h6>
+					<h5>
+						Meet Our Team
+					</h5>
+					<img src="">
+					<h4>
+						Zane Jesser
+						<span> Founder</span>
+					</h4>
+					<p>
+						Zane Jesser is the founder of Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 
+					</p>
+					<div class="socialIconsandtest">
+						<a href="#">
+							<i class="fa fa-facebook"></i>
+						</a>
+						<a href="#">
+							<i class="fa fa-twitter"></i>
+						</a>
+						<a href="#">
+							<i class="fa fa-instagram"></i>
+						</a>
+						<a href="#">
+							<i class="fa fa-linkedin"></i>
+						</a>
+					</div>
+				</div>
+				<div class="slidetest">
+					<h6>
+						Our Team
+					</h6>
+					<h5>
+						Meet Our Team
+					</h5>
+					<img src="">
+					<h4>
+						Zane Jesser
+						<span> Founder</span>
+					</h4>
+					<p>
+						Zane Jesser is the founder of Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 
+					</p>
+					<div class="socialIconsandtest">
+						<a href="#">
+							<i class="fa fa-facebook"></i>
+						</a>
+						<a href="#">
+							<i class="fa fa-twitter"></i>
+						</a>
+						<a href="#">
+							<i class="fa fa-instagram"></i>
+						</a>
+						<a href="#">
+							<i class="fa fa-linkedin"></i>
+						</a>
+					</div>
+				</div>
+				<div class="slidetest">
+					<h6>
+						Our Team
+					</h6>
+					<h5>
+						Meet Our Team
+					</h5>
+					<img src="">
+					<h4>
+						Zane Jesser
+						<span> Founder</span>
+					</h4>
+					<p>
+						Zane Jesser is the founder of Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 
+					</p>
+					<div class="socialIconsandtest">
+						<a href="#">
+							<i class="fa fa-facebook"></i>
+						</a>
+						<a href="#">
+							<i class="fa fa-twitter"></i>
+						</a>
+						<a href="#">
+							<i class="fa fa-instagram"></i>
+						</a>
+						<a href="#">
+							<i class="fa fa-linkedin"></i>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="ourresources">
+		<div class="container">
+			<h5>
+				Our Resources
+			</h5>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="boxcontainerinner">
+						<p>
+							MXene inks can be utilized to create highly conductive and strong binder free films and foils. That have a wide range of uses, From triboelectric devices to flexible batteries. Thin films of Mxenes have excellent electromagnetic absorption capabilities. See this article for more information of MXene applications.
+						</p>
+						<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/image/6.png">Read Article</a>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="boxcontainerinner">
+						<p>
+							MXene inks can be utilized to create highly conductive and strong binder free films and foils. That have a wide range of uses, From triboelectric devices to flexible batteries. Thin films of Mxenes have excellent electromagnetic absorption capabilities. See this article for more information of MXene applications.
+						</p>
+						<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/image/6.png">Read Article</a>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="boxcontainerinner">
+						<p>
+							MXene inks can be utilized to create highly conductive and strong binder free films and foils. That have a wide range of uses, From triboelectric devices to flexible batteries. Thin films of Mxenes have excellent electromagnetic absorption capabilities. See this article for more information of MXene applications.
+						</p>
+						<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/image/6.png">Read Article</a>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="boxcontainerinner">
+						<p>
+							MXene inks can be utilized to create highly conductive and strong binder free films and foils. That have a wide range of uses, From triboelectric devices to flexible batteries. Thin films of Mxenes have excellent electromagnetic absorption capabilities. See this article for more information of MXene applications.
+						</p>
+						<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/image/6.png">Read Article</a>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="boxcontainerinner">
+						<p>
+							MXene inks can be utilized to create highly conductive and strong binder free films and foils. That have a wide range of uses, From triboelectric devices to flexible batteries. Thin films of Mxenes have excellent electromagnetic absorption capabilities. See this article for more information of MXene applications.
+						</p>
+						<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/image/6.png">Read Article</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+<?php get_footer(); ?>
