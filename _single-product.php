4e17a835afa3a91bb359dc412f91be4537222b7b
mozinomer <mozinomer12@gmail.com>
2020-10-02 23:30:26 +0500

    asd

--- _single-product.php
+++ _single-product.php
@@ -1,64 +1,38 @@
-<?php 
-get_header(); 
-global $product;
+<?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
 
-while(have_posts()) : the_post();
+get_header(); ?>
 
-// $short_description  = $product->short_description; 
-// $description  = $product->description;   
-// $id  = $product->id;
-// $sku  = $product->sku;
-// $title  = $product->name;
-// $image  = $product->image_id;
-// $availability  = $product->get_stock_status();
-// $woo_price_html  =  $product->get_price_html();
-// $short_description  = apply_filters( 'woocommerce_short_description', $post->post_excerpt );
-// $sku  = $product->get_sku();
-// $price  = $product->price;
-// $regular_price  = $product->get_regular_price();
-// $sale_price  = $product->get_sale_price();
-// $post_thumbnail_id  = $product->get_image_id();
-    
-?>
-<?php //echo $product; ?>
+	<?php
+		/**
+		 * woocommerce_before_main_content hook.
+		 *
+		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+		 * @hooked woocommerce_breadcrumb - 20
+		 */
+		do_action( 'woocommerce_before_main_content' );
+	?>
 
-<?php 
+		<?php while ( have_posts() ) : ?>
+			<?php the_post(); ?>
 
+			<?php wc_get_template_part( 'content', 'single-product' ); ?>
 
-global $product;
+		<?php endwhile; // end of the loop. ?>
 
-$columns           = apply_filters( 'woocommerce_product_thumbnails_columns', 4 );
-$post_thumbnail_id = $product->get_image_id();
-$wrapper_classes   = apply_filters(
-	'woocommerce_single_product_image_gallery_classes',
-	array(
-		'woocommerce-product-gallery',
-		'woocommerce-product-gallery--' . ( $product->get_image_id() ? 'with-images' : 'without-images' ),
-		'woocommerce-product-gallery--columns-' . absint( $columns ),
-		'images',
-	)
-);
-?>
-<div class="<?php echo esc_attr( implode( ' ', array_map( 'sanitize_html_class', $wrapper_classes ) ) ); ?>" data-columns="<?php echo esc_attr( $columns ); ?>" style="opacity: 0; transition: opacity .25s ease-in-out;">
-	<figure class="woocommerce-product-gallery__wrapper">
-		<?php
-		if ( $product->get_image_id() ) {
-			$html = wc_get_gallery_image_html( $post_thumbnail_id, true );
-		} else {
-			$html  = '<div class="woocommerce-product-gallery__image--placeholder">';
-			$html .= sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src( 'woocommerce_single' ) ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
-			$html .= '</div>';
-		}
+	<?php
+		/**
+		 * woocommerce_after_main_content hook.
+		 *
+		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+		 */
+		//do_action( 'woocommerce_after_main_content' );
+	?>
 
-		echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', $html, $post_thumbnail_id ); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
+<?php
+get_footer();
 
-		do_action( 'woocommerce_product_thumbnails' );
-		?>
-	</figure>
-</div>
-<?php 
-
-endwhile;
-
-get_footer(); ?>
+/* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
