3639880ef56f6812cf42b6abb7a053ad8b823640
mozinomer <mozinomer12@gmail.com>
2020-10-02 15:08:39 +0500

    asd

--- contact.php
+++ contact.php
@@ -1,6 +1,38 @@
 <?php /* Template Name: Contact */ get_header(); ?>
 
-
+<div class="informationsetup">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="boxcontaineercontact">
+					<a href="#">
+						<i class="fa fa-phone"></i>
+						<h6>Phone Number</h6>
+						<span> 123 456 7888</span>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="boxcontaineercontact">
+					<a href="#">
+						<i class="fa fa-phone"></i>
+						<h6>Email Address</h6>
+						<span> sales@ultrionenergy.com</span>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="boxcontaineercontact">
+					<a href="#">
+						<i class="fa fa-phone"></i>
+						<h6>Address</h6>
+						<span> 1790  Russell Street, Utah 84101</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
 
 
